acd157031ee19121a1a9e7afa7c5c5dda1278c6c
Shi Zhang <slz250@nyu.edu>
2018-12-24 13:59:50 -0500

    xmas grind

---
+++ leetcode/bloomberg/other/max_sliding_window.py
@@ -0,0 +1,2 @@
+def maxSlidingWindow(nums, k):
+    pass
--- leetcode/bloomberg/trees/populate_next_right.py
+++ leetcode/bloomberg/trees/populate_next_right.py
@@ -23,21 +23,20 @@ def connectII(root):
     queue = list()
     queue.append((root,0))
     prev = root
+    prev_level = None
     curr = None
     prev.right = curr
     while queue:
         curr = queue.pop()
         node = curr[0]
         level = curr[1]
-        if node is not root and prev:
-            if prev.level != level or not queue:
-                prev.right = None
-            else:
+        if node is not root:
+            if level == prev_level:
                 prev.right = node
-        if node:
-            queue.append((node.left, level+1))
-            queue.append((node.right, level+1))
+        if node.left: queue.append((node.left, level+1))
+        if node.right: queue.append((node.right, level+1))
         prev = node
+        prev_level = level
 
 def levelOrderTraversal(root):
     """
@@ -70,3 +69,28 @@ def connect1(root):
             cur = cur.next
         #next level
         pre = pre.left
+
+def connectII1(root):
+    head, prev, cur = None, None, root
+
+    while cur:
+        #for each level
+        while cur:
+            #skipping if None
+            if cur.left:
+                if prev:
+                    prev.next = cur.left
+                else:
+                    head = cur.left
+                prev = cur.left
+            if cur.right:
+                if prev:
+                    prev.next = cur.right
+                else:
+                    head = cur.right
+                prev = cur.right
+            cur = cur.next
+
+        cur = head
+        head = None
+        prev = None
