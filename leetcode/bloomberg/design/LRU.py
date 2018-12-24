2e027a3b32619ba20ed334f4b7f378c82b78011d
Shi Zhang <slz250@nyu.edu>
2018-12-23 23:10:47 -0500

    trees during holiday weekend

--- leetcode/bloomberg/design/LRU.py
+++ leetcode/bloomberg/design/LRU.py
@@ -1,3 +1,31 @@
 class LRUCache(object):
     def __init__(self, size):
-        pass
+        self.queue = list()
+        self.size = size
+        self.eles = set()
+
+    def add(self, ele):
+        if len(self.queue) == self.size:
+            self.queue.pop()
+        self.queue.append(ele)
+        self.eles.add(ele)
+        self.size += 1
+
+    def get(self, ele):
+        if ele in self.eles:
+            self.queue.remove(ele)
+            self.queue.append(ele)
+            return ele
+        else:
+            return None
+
+    def remove(self, ele):
+        if ele in self.eles:
+            self.queue.remove(ele)
+            self.size -= 1
+
+class DLLNode(object):
+    def __init__(self, val=None, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
