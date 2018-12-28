e6cd7abdd7c0a5b6a95e4493c2dbe8cca9af33f3
Shi Zhang <slz250@nyu.edu>
2018-12-28 16:49:46 -0500

    queue stack

--- leetcode/bloomberg/design/queue_stack.py
+++ leetcode/bloomberg/design/queue_stack.py
@@ -2,16 +2,24 @@ import queue
 
 class MyStack(object):
     def __init__(self):
-        self.stack = queue.Queue()
+        self.q = queue.Queue()
 
     def push(self, x):
-        pass
+        self.q.put(x)
 
     def pop(self):
-        pass
+        num_pop = self.q.qsize()-1
+        for i in range(num_pop):
+            self.q.put(self.q.get())
+        return self.q.get()
 
     def top(self):
-        pass
+        num_pop = self.q.qsize() - 1
+        for i in range(num_pop):
+            self.q.put(self.q.get())
+        res = self.q.get()
+        self.q.put(res)
+        return res
 
     def empty(self):
-        pass
+        return self.q.empty()
---
+++ leetcode/bloomberg/dynamic_programming/word_break.py
@@ -0,0 +1,8 @@
+"""
+recognize that the problem requires to try out all different combinations
+therefore we should probably use DP
+
+
+"""
+def workBreak(s, wordDict):
+    pass
