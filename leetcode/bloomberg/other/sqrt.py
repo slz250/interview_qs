05adde22e405e46059f41ee4176dcf8bc895a239
Shi Zhang <slz250@nyu.edu>
2018-12-30 14:57:08 -0500

    sqrt

--- leetcode/bloomberg/other/sqrt.py
+++ leetcode/bloomberg/other/sqrt.py
@@ -1,2 +1,25 @@
 def mySqrt(x):
-    pass
+    if not x:
+        return None
+    low = 0
+    high = x
+    prev = x
+    while low <= high:
+        mid = int((low + high)/2)
+        print(f'low: {low} high: {high} mid: {mid}')
+        print(f'prev^2: {prev*prev}')
+        if mid*mid == x:
+            return mid
+        elif prev*prev < x < mid*mid:
+            return prev
+        elif mid*mid > x:
+            high = mid-1
+        else:
+            low = mid+1
+        prev = mid
+    return None
+
+if __name__ == '__main__':
+    x = 24
+    res = mySqrt(x)
+    print(res)
