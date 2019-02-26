1edb596670a5e6bcdbea14e850f278aad4e95a15
Shi Zhang <slz250@nyu.edu>
2019-02-26 18:09:16 -0500

    strings - longest consecutive sequence finished

--- leetcode/arrays_strings/longest_consecutive_sequence.py
+++ leetcode/arrays_strings/longest_consecutive_sequence.py
@@ -35,5 +35,27 @@ class Solution(object):
 
         return max(longest_streak, current_streak)
 
-    
+    def longestConsecutive(self, nums):
+        longest_streak = 0
+        num_set = set(nums)
+
+        for num in num_set:
+            #because if num-1 is in the set, then it would've
+            #already been included within a sequence already
+            if num-1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num+1 in num_set:
+                    current_num+=1
+                    current_streak+=1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
+
+                while
+
+
+
 
